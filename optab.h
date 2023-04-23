e6d0cce0365c4d6b952a6ee6bda2e8e4a4180e54
MercuryBarium <marcus.brulls@gmail.com>
2023-04-23 20:43:11 +0200

    Stage 5 complete again?

--- optab.h
+++ optab.h
@@ -12,6 +12,12 @@
 
 void   p_optab();                            /* display the optab     */
 
+
+void op_print_tree();
+void init_optree();
+void add_to_op(toktyp v);
+toktyp digest_expr();
+
 toktyp get_otype( toktyp op, toktyp arg1, toktyp arg2);
 
 #endif
